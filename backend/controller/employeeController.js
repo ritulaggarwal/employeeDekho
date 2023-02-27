a57bbbdaf1ade618799bfab20f26c5220756a528
Ritul Aggarwal <ritul.aggarwal2000@gmail.com>
2023-02-28 02:00:54 +0530

    Added try catch block in controller

--- backend/controller/employeeController.js
+++ backend/controller/employeeController.js
@@ -5,41 +5,65 @@ const getEmployees = asyncHandler(async (req, res) => {
     if (!req) {
         throw new Error("Request can not be empty")
     }
-    const data = await getEmployeesAccessor(req)
-    res.send(data)
+    try {
+        const data = await getEmployeesAccessor(req)
+        res.send(data)
+    } catch (err) {
+        throw new Error(`Error occurred while fetching Employees: ${err}`)
+    }
+
 })
 
 const getEmployeeWithId = asyncHandler(async (req, res) => {
     if (!req.params) {
         throw new Error("Employee Id is required to fetch employee with Id.")
     }
-    const data = await getEmployeeWithIdAccessor(req)
-    res.send(data)
+    try {
+        const data = await getEmployeeWithIdAccessor(req)
+        res.send(data)
+    } catch (err) {
+        throw new Error(`Error occurred while fetching Employee: ${err}`)
+    }
+
 })
 
 const createEmployee = asyncHandler(async (req, res) => {
     if (!req.body || !req.body.empId || !req.body.email) {
         throw new Error("EmpId and Email can not be empty")
     }
-    const data = await createEmployeeAccessor(req)
-    res.status(201).send(data)
+    try {
+        const data = await createEmployeeAccessor(req)
+        res.status(201).send(data)
+    } catch (err) {
+        throw new Error(`Error occurred while creating Employee: ${err}`)
+    }
+
 })
 
 const updateEmployee = asyncHandler(async (req, res) => {
     if (!req.params) {
         throw new Error("Employee Id can not be null")
     }
-    const data = await updateEmployeeAccessor(req)
-    res.status(200).send(data)
+    try {
+        const data = await updateEmployeeAccessor(req)
+        res.status(200).send(data)
+    } catch (err) {
+        throw new Error(`Error occurred while updating Employee: ${err}`)
+    }
 })
 
 const deleteEmployee = asyncHandler(async (req, res) => {
     if (!req.params) {
         throw new Error("Employee Id can not be null")
     }
-    const data = await deleteEmployeeAccessor(req)
-    console.log(data)
-    res.status(202).send(data)
+    try {
+        const data = await deleteEmployeeAccessor(req)
+        console.log(data)
+        res.status(202).send(data)
+    } catch (err) {
+        throw new Error(`Error occurred while deleting Employee: ${err}`)
+    }
+
 })
 
 export { getEmployees, getEmployeeWithId, createEmployee, updateEmployee, deleteEmployee } 
